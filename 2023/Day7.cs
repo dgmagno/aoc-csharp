caefab97c8a1d6368635f80ae254d339b41d0ad6
Danillo Magno <danillo.magno@zbra.dev>
2023-12-08 21:57:19 -0300

    Day 7

---
+++ 2023/Day7.cs
@@ -0,0 +1,186 @@
+﻿using System.Diagnostics;
+
+namespace aoc
+{
+    class Game : IComparer<Hand>
+    {
+        private readonly Dictionary<char, int> strengths = new()
+        {
+            ['A'] = 12,
+            ['K'] = 11,
+            ['Q'] = 10,
+            ['J'] = 9,
+            ['T'] = 8,
+            ['9'] = 7,
+            ['8'] = 6,
+            ['7'] = 5,
+            ['6'] = 4,
+            ['5'] = 3,
+            ['4'] = 2,
+            ['3'] = 1,
+            ['2'] = 0,
+        };
+
+        private readonly char? joker;
+        private readonly Hand[] hands;
+
+        public Game(Hand[] hands, char? joker = null)
+        {
+            if (joker is not null)
+            {
+                strengths[joker.Value] = -1;
+            }
+
+            this.joker = joker;
+
+            this.hands = [.. hands.Order(this)];
+        }
+
+        public int GetStrength(Hand hand, int cardPosition) => strengths[hand.Value[cardPosition]];
+
+        public CamelType GetCamelType(Hand hand)
+        {
+            var jokers = hand.Value.Count(it => it == joker);
+
+            var lists = hand.Value
+                .Where(it => it != joker)
+                .GroupBy(it => it)
+                .Select(it => it.Count())
+                .OrderDescending()
+                .ToList();
+
+            return lists switch
+            {
+                [] when jokers == 5 => CamelType.Five,
+                [1] when jokers == 4 => CamelType.Five,
+                [2] when jokers == 3 => CamelType.Five,
+                [1, 1] when jokers == 3 => CamelType.Four,
+                [3] when jokers == 2 => CamelType.Five,
+                [2, 1] when jokers == 2 => CamelType.Four,
+                [1, 1, 1] when jokers == 2 => CamelType.Three,
+                [4] when jokers == 1 => CamelType.Five,
+                [3, 1] when jokers == 1 => CamelType.Four,
+                [2, 1, 1] when jokers == 1 => CamelType.Three,
+                [2, 2] when jokers == 1 => CamelType.FulHouse,
+                [1, 1, 1, 1] when jokers == 1 => CamelType.One,
+                [5] => CamelType.Five,
+                [4, 1] => CamelType.Four,
+                [3, 2] => CamelType.FulHouse,
+                [3, 1, 1] => CamelType.Three,
+                [2, 2, 1] => CamelType.Two,
+                [2, 1, 1, 1] => CamelType.One,
+                [1, 1, 1, 1, 1] => CamelType.Zero,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public int TotalWinnings()
+        {
+            var result = 0;
+
+            for (int i = 0; i < hands.Length; i++)
+            {
+                result += hands[i].Bid * (i + 1);
+            }
+
+            return result;
+        }
+
+        public int Compare(Hand? one, Hand? other)
+        {
+            one = one ?? throw new Exception();
+            other = other ?? throw new Exception();
+
+            if (one == other)
+            {
+                return 0;
+            }
+
+            var result = Comparer<CamelType>.Default.Compare(GetCamelType(one), GetCamelType(other));
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            var i = 0;
+            int oneStrength;
+            int otherStrength;
+
+            do
+            {
+                oneStrength = GetStrength(one, i);
+                otherStrength = GetStrength(other, i);
+                i++;
+            }
+            while (oneStrength == otherStrength);
+
+            return oneStrength.CompareTo(otherStrength);
+        }
+    }
+
+    record Hand(string Value, int Bid);
+
+    enum CamelType
+    {
+        Five = 6,
+        Four = 5,
+        FulHouse = 4,
+        Three = 3,
+        Two = 2,
+        One = 1,
+        Zero = 0,
+    }
+
+    public class Day7 : IPuzzle
+    {
+        public object Part1(string[] input)
+        {
+            var hands = Parse(input);
+
+            var game = new Game(hands);
+
+            return game.TotalWinnings();
+        }
+
+        public object Part2(string[] input)
+        {
+            var hands = Parse(input);
+
+            var game = new Game(hands, 'J');
+
+            return game.TotalWinnings();
+        }
+
+        private static Hand[] Parse(string[] input)
+        {
+            return input
+                .Select(it =>
+                {
+                    var splitted = it.Split(" ");
+
+                    return new Hand(splitted[0], int.Parse(splitted[1]));
+                })
+                .ToArray();
+        }
+
+
+        public void Example()
+        {
+            var example =
+                """
+                32T3K 765
+                T55J5 684
+                KK677 28
+                KTJJT 220
+                QQQJA 483
+                """
+                .Split(Environment.NewLine);
+
+            Console.WriteLine(Part2(example));
+
+            Debug.Assert(Equals(Part1(example), 6440));
+            Debug.Assert(Equals(Part2(example), 5905));
+        }
+    }
+}
